21d3bd81eb2f93e32101854f2e0587c8f2ec526f
Dong Le <61934131+dongle47@users.noreply.github.com>
2022-06-18 13:18:23 +0700

    exclude express

--- index.js
+++ index.js
@@ -1,12 +1,63 @@
-var express = require('express');
-var jsonServer = require('json-server');
+const jsonServer = require('json-server');
+//const queryString = require('query-string');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-var server = express();
-// ...
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
 
-// You may want to mount JSON Server on a specific end-point, for example /api
-// Optiona,l except if you want to have JSON Server defaults
-// server.use('/api', jsonServer.defaults()); 
-server.use('/api', jsonServer.router('db.json'));
+// Add custom routes before JSON Server router
+server.get('/echo', (req, res) => {
+  res.jsonp(req.query);
+});
 
-server.listen(3000);
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now();
+    req.body.updatedAt = Date.now();
+  } else if (req.method === 'PATCH') {
+    req.body.updatedAt = Date.now();
+  }
+
+  // Continue to JSON Server router
+  next();
+});
+
+// Custom output for LIST with pagination
+router.render = (req, res) => {
+  // Check GET with pagination
+  // If yes, custom output
+  const headers = res.getHeaders();
+
+  const totalCountHeader = headers['x-total-count'];
+  if (req.method === 'GET' && totalCountHeader) {
+    const queryParams = queryString.parse(req._parsedUrl.query);
+
+    const result = {
+      data: res.locals.data,
+      pagination: {
+        _page: Number.parseInt(queryParams._page) || 1,
+        _limit: Number.parseInt(queryParams._limit) || 10,
+        _totalRows: Number.parseInt(totalCountHeader),
+      },
+    };
+
+    return res.jsonp(result);
+  }
+
+  // Otherwise, keep default behavior
+  res.jsonp(res.locals.data);
+};
+
+// Use default router
+server.use('/api', router);
+
+// Start server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log('JSON Server is running');
+});
