2f2423f513eb76d6a4f333437333ceeca1120f18
Dong Le <61934131+dongle47@users.noreply.github.com>
2022-06-18 11:34:02 +0700

    integrate with json server

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const jsonServer = require('json-server');
 const app = express()
 
 let port = process.env.PORT || 3000;
@@ -7,15 +8,18 @@ app.get("/", (req, res) => {
     res.send("Hello world")
 })
 
-const citiesData = require("./cities.json")
-app.get("/cities", (req, res) => {
-    res.send(citiesData)
-})
+// const citiesData = require("./cities.json")
+// app.get("/cities", (req, res) => {
+//     res.send(citiesData)
+// })
 
-const studentsData = require("./students.json")
-app.get("/students", (req, res) => {
-    res.send(studentsData)
-})
+// const studentsData = require("./students.json")
+// app.get("/students", (req, res) => {
+//     res.send(studentsData)
+// })
+
+app.use('/cities', jsonServer.router('cities.json'));
+app.use('/students', jsonServer.router('students.json'));
 
 app.listen(port, () => {
     console.log(`Example app is listening on port http://localhost:${port}`)
