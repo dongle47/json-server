ae4cad50b9e4165fcc93b77ba5a76dbd85c65a48
Dong Le <61934131+dongle47@users.noreply.github.com>
2022-08-09 10:35:18 +0700

    fix

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ router.render = (req, res) => {
 server.use('/api', router);
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log('JSON Server is running');
 });
